77f00ea857ee7a0ae4a840f2b49da7305b43e2d4
lianet <lianet@front10.com>
2023-09-10 23:59:36 -0400

    feat(not-found) not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,28 @@
+import {Box, Typography} from '@/lib/mui';
+import React from "react";
+import Image from "next/image";
+
 export default function Custom404() {
-    return <h1>Sorry, this page does not exists</h1>;
+    return <Box sx={{
+        width: '100%',
+        maxWidth: '1366px',
+        mx: 'auto',
+        overflow: 'hidden',
+        position: 'relative',
+        zIndex: 1,
+        color: 'white',
+        pt: 2,
+        px: '1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }}>
+        <Typography variant='h4' sx={{my: 8, textAlign: 'center'}}>Sorry, this page does not exists</Typography>
+        <Image
+            src="/not-found.png"
+            width={500}
+            height={500}
+            alt="not-found"
+        />
+    </Box>
 }

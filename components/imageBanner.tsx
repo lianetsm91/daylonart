b29a10d564cf5feadcc693c246b9c5c91796100e
lianet <lianet@front10.com>
2024-04-03 12:54:13 -0400

    refactor(image-banner): remove unnecessary code

--- components/imageBanner.tsx
+++ components/imageBanner.tsx
@@ -4,49 +4,6 @@ import Image from 'next/image';
 import './imageBanner.css';
 import { Box } from '@ui/Box';
 
-const handleScroll = () => {
-  const imageList = document.querySelectorAll('#image-list-container');
-  const bannerImage = document.querySelectorAll('.banner-image');
-  const bannerImageContainer = document.querySelectorAll('.image-banner-container');
-
-  const elementTop = imageList[0]?.getBoundingClientRect()?.top;
-
-  //todo refactor and remove extra lines
-  if (elementTop < 400 && elementTop > 250) {
-    bannerImage[0].classList.add('transition1');
-    bannerImage[0].classList.remove('transition2');
-    bannerImage[0].classList.remove('transition3');
-    bannerImageContainer[0].classList.add('transition1');
-    bannerImageContainer[0].classList.remove('transition2');
-    bannerImageContainer[0].classList.remove('transition3');
-    return;
-  }
-  if (elementTop < 250 && elementTop > 100) {
-    bannerImage[0].classList.add('transition2');
-    bannerImage[0].classList.remove('transition1');
-    bannerImage[0].classList.remove('transition3');
-    bannerImageContainer[0].classList.add('transition2');
-    bannerImageContainer[0].classList.remove('transition1');
-    bannerImageContainer[0].classList.remove('transition3');
-    return;
-  }
-  if (elementTop < 100) {
-    bannerImage[0].classList.add('transition3');
-    bannerImage[0].classList.remove('transition1');
-    bannerImage[0].classList.remove('transition2');
-    bannerImageContainer[0].classList.add('transition3');
-    bannerImageContainer[0].classList.remove('transition1');
-    bannerImageContainer[0].classList.remove('transition2');
-    return;
-  }
-  bannerImage[0].classList.remove('transition1');
-  bannerImage[0].classList.remove('transition2');
-  bannerImage[0].classList.remove('transition3');
-  bannerImageContainer[0].classList.remove('transition1');
-  bannerImageContainer[0].classList.remove('transition2');
-  bannerImageContainer[0].classList.remove('transition3');
-};
-
 export function ImageBanner() {
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -55,6 +12,35 @@ export function ImageBanner() {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const handleScroll = () => {
+    const imageList = document.querySelectorAll('#image-list-container');
+    const bannerImage = document.querySelectorAll('.banner-image');
+
+    const elementTop = imageList[0]?.getBoundingClientRect()?.top;
+
+    if (elementTop < 400 && elementTop > 250) {
+      bannerImage[0].classList.add('transition1');
+      bannerImage[0].classList.remove('transition2');
+      bannerImage[0].classList.remove('transition3');
+      return;
+    }
+    if (elementTop < 250 && elementTop > 100) {
+      bannerImage[0].classList.add('transition2');
+      bannerImage[0].classList.remove('transition1');
+      bannerImage[0].classList.remove('transition3');
+      return;
+    }
+    if (elementTop < 100) {
+      bannerImage[0].classList.add('transition3');
+      bannerImage[0].classList.remove('transition1');
+      bannerImage[0].classList.remove('transition2');
+      return;
+    }
+    bannerImage[0].classList.remove('transition1');
+    bannerImage[0].classList.remove('transition2');
+    bannerImage[0].classList.remove('transition3');
+  };
 
   return (
     <Box className="image-banner-container">
